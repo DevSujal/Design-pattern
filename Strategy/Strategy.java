3681d04449f673c7be8eb8a58724dc9afd93f2c5
Sujal Sunil Nimje <153418285+DevSujal@users.noreply.github.com>
2024-10-15 09:09:07 +0000

    strategy code completed

--- Strategy/Strategy.java
+++ Strategy/Strategy.java
@@ -1,42 +1,133 @@
 import java.util.*;
 
-interface SortingTechniques{
-  void sort();
+interface SortingTechniques {
+    void sort(int[] arr);
 }
 
-class BubbleSort implements SortingTechniques{
-  public void sort(){
-    System.out.println("You are in Bubble sort");
-  }
+class BubbleSort implements SortingTechniques {
+    public void sort(int[] arr) {
+      System.out.println("sorting using bubble sort strategy!");
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
 
-class SelectionSort implements SortingTechniques{
-  public void sort(){
-    System.out.println("You are in Selection sort");
-  }
+class SelectionSort implements SortingTechniques {
+    public void sort(int[] arr) {
+      System.out.println("sorting using selection sort strategy!");
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
 }
 
-class InsertionSort implements SortingTechniques{
-  public void sort(){
-    System.out.println("You are in Insertion sort");
-  }
+class InsertionSort implements SortingTechniques {
+    public void sort(int[] arr) {
+      System.out.println("sorting using insertion sort strategy!");
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
 
-class QuickSort implements SortingTechniques{
-  public void sort(){
-    System.out.println("You are in Quick sort");
-  }
+class QuickSort implements SortingTechniques {
+    public void sort(int[] arr) {
+      System.out.println("sorting using quick sort strategy!");
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // Swap arr[i + 1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
 }
 
-class MergeSort implements SortingTechniques{
-  public void sort(){
-    System.out.println("You are in Merge sort");
+class MergeSort implements SortingTechniques {
+  public void sort(int[] arr) {
+    System.out.println("sorting using merge sort strategy!");
+    divide(arr);
   }
+    public void divide(int[] arr) {
+        if (arr.length < 2) return;
+        int mid = arr.length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        divide(left);
+        divide(right);
+        merge(arr, left, right);
+    }
+
+    private void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
 }
 
-public class Strategy{
-  public static void main(String[] args){
-    SortingTechniques b = new BubbleSort();
-    b.sort();
-  }
+public class Strategy {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+
+        SortingTechniques sorter = new MergeSort(); // Change to any sorting algorithm
+        sorter.sort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
 }
